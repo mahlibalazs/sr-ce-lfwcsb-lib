d76cc7850a117fc15f272a21a0fa2d7e4faa8840
Balázs Máhli <mahli.balazs@outlook.at>
2026-03-25 19:35:07 +0100

    feat: Add Scoreboard implementation

--- src/main/java/Scoreboard.java
+++ src/main/java/Scoreboard.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Scoreboard {
@@ -10,19 +14,32 @@ public class Scoreboard {
     }
 
     public void startMatch(String homeTeam, String awayTeam) {
-        // TODO
+        matches.put(key(homeTeam, awayTeam), new Match(homeTeam, awayTeam));
     }
 
     public void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore) {
-        // TODO
+        Match match = matches.get(key(homeTeam, awayTeam));
+        if (match == null) {
+            throw new IllegalArgumentException("Match not found");
+        }
+        match.setHomeScore(homeScore);
+        match.setAwayScore(awayScore);
     }
 
     public void finishMatch(String homeTeam, String awayTeam) {
-        // TODO
+        matches.remove(key(homeTeam, awayTeam));
     }
 
-    public void summary() {
-        // TODO
+    public String summary() {
+        List<Match> list = new ArrayList<>(matches.values());
+
+        Collections.reverse(list);
+
+        list.sort(Comparator.comparingInt(Match::getTotalScore).reversed());
+
+        StringBuilder sb = new StringBuilder();
+        list.forEach(match -> sb.append(match).append("\n"));
+        return sb.toString();
     }
 
 }
